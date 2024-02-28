2aba14b926c4c7b0225074396587678c23c544d8
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-02-28 22:21:17 +0100

    feat: function to format account number

---
+++ PayFlow/utils/formatAccountNumber.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export function formatAccountNumber(accountNumber: string): string {
+  if (!accountNumber) {
+    return '';
+  }
+
+  const accountString: string = accountNumber.toString();
+  const firstTwoDigits: string = accountString.substring(0, 2);
+  const remainingDigits: string = accountString.substring(2);
+
+  const chunks: string[] = [];
+  for (let i = 0; i < remainingDigits.length; i += 4) {
+    chunks.push(remainingDigits.substring(i, i + 4));
+  }
+
+  return `${firstTwoDigits} ${chunks.join(' ')}`;
+}
