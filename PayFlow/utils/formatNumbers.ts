7b049f11aa544c2ea1883aa0796ccabe7082a7d1
enqer <pawel.kam4@gmail.com>
2024-03-14 12:27:35 +0100

    feat: formatted account number

--- PayFlow/utils/formatNumbers.ts
+++ PayFlow/utils/formatNumbers.ts
@@ -36,3 +36,20 @@ export function formatExpirationDate(dateString: string): string {
   const lastTwoDigitsOfYear = year.slice(-2);
   return `${month}/${lastTwoDigitsOfYear}`;
 }
+
+export function formatAccountNumber(accountNumber: string | undefined) {
+  if (!accountNumber) {
+    return '';
+  }
+
+  const accountString = accountNumber.toString();
+  const firstTwoDigits = accountString.substring(0, 2);
+  const remainingDigits = accountString.substring(2);
+
+  const chunks = [];
+  for (let i = 0; i < remainingDigits.length; i += 4) {
+    chunks.push(remainingDigits.substring(i, i + 4));
+  }
+
+  return `${firstTwoDigits} ${chunks.join(' ')}`;
+}
